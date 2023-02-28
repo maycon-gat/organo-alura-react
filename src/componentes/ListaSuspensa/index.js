dd83d28419fa3f48b1d007c1d8130c840c5f5b64
Maycon Henrique Gat <83017918+maycon-gat@users.noreply.github.com>
2023-02-28 15:40:16 -0300

    first

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,21 @@
+import "./ListaSuspensa.css";
+
+const ListaSuspensa = (props) => {
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select
+        onChange={(e) => props.aoAlterado(e.target.value)}
+        required={props.obrigatorio}
+        value={props.valor}
+      >
+        <option key="empty" value=""></option>
+        {props.itens.map((item) => {
+          return <option key={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,11 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <header className="rodape">
+      <img src="/imagens/rodape.png" alt="O rodape da página Organo"></img>
+    </header>
+  );
+};
+
+export default Rodape;
